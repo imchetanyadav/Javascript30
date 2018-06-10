73b77bd66223859e4bd1367599a0c2209c1edb19
Chetan Yadav <imchetanyadav@gmail.com>
2018-06-11 04:52:09 +0530

    Day 11

---
+++ Day 11/script.js
@@ -0,0 +1,11 @@
+const pressed = [];
+const secretCode = 'chetan';
+
+window.addEventListener('keyup', (e) => {
+  pressed.push(e.key);
+  pressed.slice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if(pressed.join('').includes(secretCode)){
+    console.log('Yep thats me.')
+    cornify_add();
+  }
+});
